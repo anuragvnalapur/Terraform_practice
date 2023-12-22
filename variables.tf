fa60eafe7a6f6472446889c498de27d4bdcc94fe
root <root@ip-172-31-16-63.ec2.internal>
2023-12-22 08:46:16 +0000

    2 files added

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type_value" {
+
+	description = "Instance type "
+}
+
+
+variable "subnet_id_value" {
+	description = "subnet ID"
+	
+}
+
+variable "ami_value" {
+
+	description = "ami id"
+}
